bb4d1ca8f441e00748ba6a26d39d8aad575ef3e1
Ca-Jou <camille.pauron@gmail.com>
2020-12-10 17:53:28 +0100

    added map to query string conversion function

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  userQuery = new Map<any, any>();
+  userQuery = new Map<string, string>();
   userTags: string = '';
   tailleImg: string = '';
   dateMin: string = '';
@@ -20,17 +20,17 @@ export class SearchComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  search() {
+  search(): string {
     this.userTags = this.userTags.replace(' ', '+');
     this.userQuery.set('tags', this.userTags);
 
     if (this.dateMin != '') {
-      const dateMinStamp = Math.round(new Date(this.dateMin.replace('-', '/')).getTime() / 1000);
+      const dateMinStamp = Math.round(new Date(this.dateMin.replace('-', '/')).getTime() / 1000).toString();
       this.userQuery.set('min_upload_date', dateMinStamp);
     }
 
     if (this.dateMax != '') {
-      const dateMaxStamp = Math.round(new Date(this.dateMax.replace('-', '/')).getTime() / 1000);
+      const dateMaxStamp = Math.round(new Date(this.dateMax.replace('-', '/')).getTime() / 1000).toString();
       this.userQuery.set('max_upload_date', dateMaxStamp);
     }
 
@@ -49,9 +49,17 @@ export class SearchComponent implements OnInit {
       this.userQuery.set('height', this.tailleImg);
       this.userQuery.set('width', this.tailleImg);
     }
+    return this.convert(this.userQuery);
+  }
 
-    console.log(this.userQuery);
-    return this.userQuery;
+  convert(userQuery: Map<string, string>): string {
+    let query: string = '';
+
+    for (const [key, value] of userQuery) {
+      query += '&' + key + '=' + value;
+    }
+
+    return query;
   }
 
 }
